c86f51b9c263b1321aa875317e1666d4f1e8cb10
Alex de Wergifosse <alex@edgeworx.io>
2021-11-29 17:04:59 +1300

    Add development env variable

--- src/ui/src/App.tsx
+++ src/ui/src/App.tsx
@@ -5,11 +5,14 @@ import './App.scss'
 
 import theme from './Theme'
 
+const baseURL = process.env.REACT_APP_BASE_URL ?? 'http://localhost'
+
 function App () {
   return (
     <div className='App'>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <img src={`${baseURL}:3456/`} alt='live-feed' />
       </ThemeProvider>
     </div>
   )
