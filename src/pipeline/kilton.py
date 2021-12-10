fe12f13bcd0163d3039ce9b4436ec83184421e83
Alex de Wergifosse <alex@edgeworx.io>
2021-12-10 13:03:37 +1300

    Add kilton

--- src/pipeline/kilton.py
+++ src/pipeline/kilton.py
@@ -6,30 +6,18 @@ from .basic_perceptor import BasicPerceptor
 from darcyai_coral.people_perceptor import PeoplePerceptor
 import os
 import time
-import platform
-
 
 absolutepath = os.path.dirname(os.path.abspath(__file__))
 
-def is_mac_osx():
-    return platform.system() == "Darwin"
-
-def get_input_stream(input):
-    if input["type"] == "video_file":
-        return VideoFileStream(os.path.join(absolutepath, input["file"]))
-    elif input["type"] == "live_feed":
-        return CameraStream(video_device=0 if is_mac_osx() else "/dev/video0")
-    else:
-        return CameraStream(video_device=0 if is_mac_osx() else "/dev/video0")
-
 class ExplorerPipeline():    
-    def __init__(self, app, input, event_cb):
+    def __init__(self):
         self.__stopped = False
-        self.__pipeline = Pipeline(input_stream=get_input_stream(input),
+        self.__pipeline = Pipeline(input_stream=VideoFileStream(os.path.join(absolutepath, "video.mp4")),
                                    universal_rest_api=True,
-                                   rest_api_flask_app=app,
-                                   rest_api_base_path="/pipeline")
-        self.__output_stream = LiveFeedStream(flask_app=app, path="/live_feed")
+                                   host="0.0.0.0",
+                                   port=8080,
+                                   rest_api_base_path="/")
+        self.__output_stream = LiveFeedStream(host="0.0.0.0", port="3456", path="/live_feed")
         self.__pipeline.add_output_stream("live_feed", self.__output_stream_callback, self.__output_stream)
 
         self.__summary = {
@@ -38,7 +26,6 @@ class ExplorerPipeline():
             "faceMasks": 0,
             "qrCodes": 0
         }
-        self.__event_cb = event_cb
 
         # People Perceptor
         self.__people_perceptor_name = "people"
@@ -53,12 +40,6 @@ class ExplorerPipeline():
         self.__pipeline.set_perceptor_config(self.__people_perceptor_name, "face_rectangle_color", "255,255,255")
         self.__pipeline.set_perceptor_config(self.__people_perceptor_name, "show_person_id", True)
 
-        # Basic Perceptor
-        basic_perceptor = BasicPerceptor()
-        self.__pipeline.add_perceptor("basic", basic_perceptor, parent=self.__people_perceptor_name, accelerator_idx=0, input_callback=self.__perceptor_input_callback)
-        ## Event callbacks
-        basic_perceptor.on("event_1", event_cb("basic", "event_1"))
-
     def __reset_summary(self):
         self.__summary = {
             "inScene": 0,
@@ -69,7 +50,6 @@ class ExplorerPipeline():
 
     def __on_new_person_entered_scene(self, event_data):
         self.__summary["visitors"] += 1
-        self.__event_cb(self.__people_perceptor_name, "new_person_entered_scene")(event_data)
 
     def run(self):
         while True:
@@ -87,13 +67,6 @@ class ExplorerPipeline():
         # TODO: Move this logic to pulse_completion_callback
         self.__summary["inScene"] = pom.get_perceptor(self.__people_perceptor_name).peopleCount()
         return input_data.data.copy()
-
-    def change_input(self, input):
-        self.__stopped = True
-        self.__pipeline.stop()
-        self.__pipeline.update_input_stream(get_input_stream(input))
-        self.__reset_summary()
-        self.__stopped = False
 
     # Passthrough callback
     def __perceptor_input_callback(self, input_data, pom, config):
