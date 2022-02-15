7e3012f270771f53f9c86a200d4787614c7be1b9
Saeid Rezaei Baghbidi <saeid@edgeworx.io>
2022-02-15 12:52:31 -0800

    Performance improvements

--- src/pipeline/explorer_pipeline.py
+++ src/pipeline/explorer_pipeline.py
@@ -62,6 +62,7 @@ class ExplorerPipeline():
         self.__pipeline.set_perceptor_config(self.__people_perceptor_name, "face_rectangle_color", "255,255,255")
         self.__pipeline.set_perceptor_config(self.__people_perceptor_name, "show_person_id", True)
         self.__pipeline.set_perceptor_config(self.__people_perceptor_name, "person_data_identity_text_font_size", 0.5)
+        self.__pipeline.set_perceptor_config(self.__people_perceptor_name, "object_tracking_allowed_missed_frames", 15)
 
         # QRCode Perceptor
         self.__qrcode_perceptor_name = "qrcode"
@@ -74,6 +75,8 @@ class ExplorerPipeline():
         self.__pipeline.add_perceptor(self.__face_mask_perceptor_name, face_mask_perceptor, accelerator_idx=0, parent=self.__people_perceptor_name, input_callback=self.__face_mask_input_callback, multi=True)
         # Update configuration
         self.__pipeline.set_perceptor_config(self.__face_mask_perceptor_name, "threshold", 0.95)
+
+        self.__qrcode_person_id = None
 
     def __update_masks_count(self, pom):
         mask_results = pom.get_perceptor(self.__face_mask_perceptor_name)
@@ -158,6 +161,12 @@ class ExplorerPipeline():
         # Pipeline has completed
         self.__latest_pom = pom
 
+        people_perceptor_pom = pom.get_perceptor(self.__people_perceptor_name)
+        qrcode_perceptor_pom = pom.get_perceptor(self.__qrcode_perceptor_name)
+        poi = self.__get_poi(people_perceptor_pom)
+        if poi is not None and len(qrcode_perceptor_pom.get_qrcodes()) > 0:
+            self.__qrcode_person_id = poi["person_uuid"]
+
     def __reset_summary(self):
         self.__summary = {
             "inScene": 0,
@@ -216,11 +225,37 @@ class ExplorerPipeline():
 
     # Only run QRCode perceptor if we have a person in front
     def __qr_code_input_callback(self, input_data, pom, config):
-        peeps = pom.get_perceptor(self.__people_perceptor_name).peopleCount()
-        if peeps > 0:
-            return input_data.data.copy()
+        people_perceptor_pom = pom.get_perceptor(self.__people_perceptor_name)
+        peeps = people_perceptor_pom.peopleCount()
 
-        return None
+        if peeps == 0:
+            return None
+
+        poi = self.__get_poi(people_perceptor_pom)
+        if poi is None or not poi["has_face"] or self.__get_face_height(poi) < 220:
+            return None
+
+        if poi["person_uuid"] == self.__qrcode_person_id:
+            return None
+
+        return input_data.data.copy()
+
+    def __get_poi(self, people_perceptor_pom):
+        peeps = people_perceptor_pom.people()
+        poi = None
+        for person in peeps:
+            if peeps[person]["is_poi"]:
+                poi = peeps[person]
+                break
+
+        return poi
+
+    def __get_face_height(self, person):
+        if not person["has_face"]:
+            return 0
+
+        rectangle = person["face_rectangle"]
+        return rectangle[1][1] - rectangle[0][1]
 
     def get_pom(self):
         return self.__pipeline.get_pom()
