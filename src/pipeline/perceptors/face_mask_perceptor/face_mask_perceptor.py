02440079a04cffebec02c0795b45ddd224b3c8c4
Alex de Wergifosse <alex@edgeworx.io>
2022-03-16 12:03:52 +1300

    debug mask count

--- src/pipeline/perceptors/face_mask_perceptor/face_mask_perceptor.py
+++ src/pipeline/perceptors/face_mask_perceptor/face_mask_perceptor.py
@@ -62,8 +62,9 @@ class FaceMaskPerceptor(ImageClassificationPerceptor):
             try:
                 idx = perception_result[1].index("Mask")
                 threshold = self.get_config_value("threshold") / 100
+                print("threshold:", threshold)
+                print("perception result:", perception_result[0][idx])
                 has_mask = bool(perception_result[0][idx][1] >= threshold)
-                print("perception result", json.dumps(perception_result[0][idx]))
                 if has_mask:
                     self.emit(RAW_MASK_EVENT, person_id)
                 else:
